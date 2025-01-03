7c4c51dba9fcb4f8ff9dfd748ceaddbc9a8fa859
devendra <dev.kumar89.dp@gmail.com>
2025-01-03 19:23:39 +0530

    Update js-ticky.js

--- js-ticky.js
+++ js-ticky.js
@@ -1,3 +1,272 @@
+
+Qustion : difference between null and undefined 
+  undefined
+    - A variable that has been declared but not assigned a value is automatically assigned the value undefined.
+    - undefined is its own type.
+
+    let x;
+    console.log(x); // undefined, because x is declared but not assigned
+
+    function foo() {
+    // no return statement
+    }
+    console.log(foo()); // undefined, because foo() doesn't return anything
+
+    const obj = {};
+    console.log(obj.nonExistentProperty); // undefined, because property doesn't exist
+
+  null
+    - null represents the intentional absence of any object value. It is explicitly assigned to variables to indicate that they should not have a value (or that they are "empty").
+    - null is of type object (this is a known quirk in JavaScript).
+
+    let x = null;
+    console.log(x); // null, explicitly assigned
+
+    let obj = { name: "John" };
+    obj = null; // clear the reference to the object
+    console.log(obj); // null, explicitly set to null
+
+
+    example comparison
+    let a;
+    let b = null;
+
+    console.log(a === undefined); // true, a is undefined
+    console.log(b === null);      // true, b is null
+
+    console.log(a == b);          // true, because undefined == null (loose equality)
+    console.log(a === b);         // false, because undefined and null are of different types
+------------
+
+
+console.log(1 + '1'); // 11
+console.log('1' + 1); // 11
+console.log(1 + + '1'); // 2 
+console.log(+'1' + 1); // 2
+// + '1' becomes 1, and then you add 1 + 1, which gives 2.
+
+
+------------
+
+let x = 10;
+let y = '10';
+
+console.log(x == y); // true - number and string check value is same or not
+console.log(x === y); // false
+
+
+------------
+console.log(0 == false); // true
+console.log(0 === false); // false
+console.log('' == false); // true
+console.log('' === false); // false
+
+------------
+
+function outer() {
+  let count = 0;
+  return function inner() {
+    count++;
+    console.log(count);
+  };
+}
+
+const first = outer();
+const second = outer();
+
+first();  // 1
+first();  // 2
+second(); // 1
+first();  // 3
+
+------------
+
+function outer() {
+  var x = 10;
+  
+  function inner() {
+    console.log(x);
+  }
+  
+  return inner;
+}
+
+var fn = outer();
+fn();  // 10  - closure's environment concept
+// Note : variable me save he to inner function call hoga and return 10 karega, bina variable ke sirf inner function return karega
+
+---------------------
+function outer() {
+  var x = 10;
+  
+  setTimeout(function() {
+    console.log(x);
+  }, 0);
+  
+  x = 20;
+}
+
+outer(); // 20
+---------------------
+var a = 1;
+
+function foo() {
+  console.log(a); // undefined  - local scope
+  var a = 2;
+}
+
+foo();
+
+
+---------------------
+function outer() {
+  var x = 1;
+  
+  function inner() {
+    var x = 2;
+    console.log(x); // 2
+  }
+  
+  inner();
+  console.log(x); // 1
+}
+
+outer();
+
+
+---------------------
+
+function outer() {
+  var x = 1;
+  
+  function inner() {
+    console.log(x); // 2
+  }
+  
+  setTimeout(inner, 1000);
+  
+  x = 2;
+}
+
+outer();
+
+
+---------------------
+
+console.log(foo()); // Typeerror : foo is not a function
+
+var foo = function() {
+  return "Hello, World!";
+};
+console.log(foo()); // Hello, World!
+
+---------------------
+function createFunctions() {
+  var functions = [];
+  
+  for (var i = 0; i < 3; i++) {
+    functions[i] = function() {
+      console.log(i);
+    };
+  }
+  
+  return functions;
+}
+
+var funcs = createFunctions();
+funcs[0](); // 3
+funcs[1](); // 3
+funcs[2](); // 3
+
+---------------------
+function createFunctions() {
+  var functions = [];
+  
+  for (let i = 0; i < 3; i++) {
+    functions[i] = function() {
+      console.log(i);
+    };
+  }
+  
+  return functions;
+}
+
+var funcs = createFunctions();
+funcs[0](); // 0
+funcs[1](); // 1
+funcs[2](); // 2
+---------------------
+for (var i = 0; i < 3; i++) {
+  setTimeout(function() {
+    console.log(i);
+  }, 1000);
+}
+// output : 333
+
+for (let i = 0; i < 3; i++) {
+  setTimeout(function() {
+    console.log(i);
+  }, 1000);
+}
+// output : 012
+---------------------
+
+
+console.log(a); // function return
+
+var a = 10;
+
+function a() {
+  console.log("Hello!");
+}
+---------------------
+const a = [];
+const b = [];
+
+a[0] = b;
+b[0] = a;
+
+console.log(a[0][0]); // Error: Maximum call stack size exceeded
+
+------------
+
+console.log([] == []); // false
+console.log([] === []); // false
+console.log({} == {}); // false
+console.log({} === {}); // false
+
+------------
+
+var a = 1;
+function foo() {
+  console.log(a);
+  var a = 2;
+}
+foo(); // undefined
+
+
+------------
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+------------
+console.log(1 + '1'); // 11 // except + operator convert string to number
+console.log('1' + 1); // 11 // except + operator convert string to number
+console.log(1 - '1'); // 0 // -, /, and * this operation convert string to number
+console.log('1' - 1); // 0 // -, /, and * this operation convert string to number
+
+------------
+
+var a = [1, 2, 3];
+var b = a;
+b[0] = 99;
+
+console.log(a); // [ 99, 2, 3 ]
+console.log(b); // [ 99, 2, 3 ]
+// b does not create a new copy of the array. Instead, it copies the reference to the array a into b. So, both a and b now refer to the same array in memory.
+------------
+
 given a string, reverse each word in the sentence
 ----------------------
     var str = "saare jahn se acha hindustan hamara"
@@ -260,53 +529,6 @@ const newObj = { prop: 2 };
 
 console.log(obj.method.call(newObj)); // 2
 
-------------
-
-const a = [];
-const b = [];
-
-a[0] = b;
-b[0] = a;
-
-console.log(a[0][0]); // Error: Maximum call stack size exceeded
-
-------------
-
-console.log([] == []); // false
-console.log([] === []); // false
-console.log({} == {}); // false
-console.log({} === {}); // false
-
-------------
-
-var a = 1;
-function foo() {
-  console.log(a);
-  var a = 2;
-}
-foo(); // undefined
-
-
-------------
-
-console.log(typeof null); // object
-console.log(typeof undefined); // undefined
-
-------------
-console.log(1 + '1'); // 11 // except + operator convert string to number
-console.log('1' + 1); // 11 // except + operator convert string to number
-console.log(1 - '1'); // 0 // -, /, and * this operation convert string to number
-console.log('1' - 1); // 0 // -, /, and * this operation convert string to number
-
-------------
-
-var a = [1, 2, 3];
-var b = a;
-b[0] = 99;
-
-console.log(a); // [ 99, 2, 3 ]
-console.log(b); // [ 99, 2, 3 ]
-// b does not create a new copy of the array. Instead, it copies the reference to the array a into b. So, both a and b now refer to the same array in memory.
 
 ------------ 
 console.log([] + []); // ''
@@ -314,6 +536,7 @@ console.log([] + {}); // [object Object]
 console.log({} + []); // [object Object]
 console.log({} + {}); // [object Object]
 console.log([] == ![]); // true
+console.log([] === ![]); // false
 //  {} is evaluated as an empty object, which is converted to the string "[object Object]".
 ------------
 
@@ -339,9 +562,19 @@ const obj = {
   const newObj = obj;
   newObj.increment();
   
-  console.log(obj.value);
+  console.log(obj.value); // 11
 // You create a new variable newObj and assign it the reference to obj. This means newObj and obj now refer to the same object in memory. So, any change to newObj will also affect obj because they point to the same underlying object.
+  ------------
+  const obj = {
+    value: 10,
+    increment() {
+      return this.value++;
+    }
+  };
   
+  const newObj = obj;
+  newObj.increment();
+  console.log(newObj.increment()); // 11 // in function without return it will be undefined
 
 ------------
 let a = 1;
@@ -351,309 +584,8 @@ a = 2;
 console.log(b); // 1
 // Note that this is not a reference assignment, since a is a primitive value (a number). So, b gets a copy of the value of a, which is 1. Now, b = 1 and a = 1.
 
-------------
-Qustion : difference between null and undefined 
-  undefined
-    - A variable that has been declared but not assigned a value is automatically assigned the value undefined.
-    - undefined is its own type.
-
-    let x;
-    console.log(x); // undefined, because x is declared but not assigned
-
-    function foo() {
-    // no return statement
-    }
-    console.log(foo()); // undefined, because foo() doesn't return anything
-
-    const obj = {};
-    console.log(obj.nonExistentProperty); // undefined, because property doesn't exist
-
-  null
-    - null represents the intentional absence of any object value. It is explicitly assigned to variables to indicate that they should not have a value (or that they are "empty").
-    - null is of type object (this is a known quirk in JavaScript).
-
-    let x = null;
-    console.log(x); // null, explicitly assigned
-
-    let obj = { name: "John" };
-    obj = null; // clear the reference to the object
-    console.log(obj); // null, explicitly set to null
-
-
-    example comparison
-    let a;
-    let b = null;
-
-    console.log(a === undefined); // true, a is undefined
-    console.log(b === null);      // true, b is null
-
-    console.log(a == b);          // true, because undefined == null (loose equality)
-    console.log(a === b);         // false, because undefined and null are of different types
-
-
-
-------------
-
-console.log(1 + '1'); // 11
-console.log('1' + 1); // 11
-console.log(1 + + '1'); // 2 
-console.log(+'1' + 1); // 2
-// + '1' becomes 1, and then you add 1 + 1, which gives 2.
-
-
-------------
-
-let x = 10;
-let y = '10';
-
-console.log(x == y); // true - number and string check value is same or not
-console.log(x === y); // false
-
-
-------------
-console.log(0 == false); // true
-console.log(0 === false); // false
-console.log('' == false); // true
-console.log('' === false); // false
-
-------------
-
-function outer() {
-  let count = 0;
-  return function inner() {
-    count++;
-    console.log(count);
-  };
-}
-
-const first = outer();
-const second = outer();
-
-first();  // 1
-first();  // 2
-second(); // 1
-first();  // 3
-
-------------
-
-
-------------
-
-------------
-
-
-
-------------
-
-
-------------
-
-------------
-
-
-
-------------
-
-
-------------
-
-------------
-
-
-
-------------
-
-
-------------
-
-------------
-
-
-
-------------
-
-
-------------
-
-------------
-
-
-
-------------
-
-
-------------
-
-------------
-
-
-
-------------
-
-
-------------
-
-------------
-
-
-
-------------
-
-
-------------
-
-------------
-
-
-
-------------
-
-
-------------
-
-------------
-
-
-
-------------
-
-
-------------
-
-------------
-
-
-
-------------
-function outer() {
-  var x = 10;
-  
-  function inner() {
-    console.log(x);
-  }
-  
-  return inner;
-}
-
-var fn = outer();
-fn();  // 10
-
-
----------------------
-function outer() {
-  var x = 10;
-  
-  setTimeout(function() {
-    console.log(x);
-  }, 0);
-  
-  x = 20;
-}
-
-outer(); // 20
----------------------
-var a = 1;
-
-function foo() {
-  console.log(a); // undefined  - local scope
-  var a = 2;
-}
-
-foo();
-
-
----------------------
-function outer() {
-  var x = 1;
-  
-  function inner() {
-    var x = 2;
-    console.log(x); // 2
-  }
-  
-  inner();
-  console.log(x); // 1
-}
-
-outer();
-
-
 ---------------------
 
-function outer() {
-  var x = 1;
-  
-  function inner() {
-    console.log(x); // 2
-  }
-  
-  setTimeout(inner, 1000);
-  
-  x = 2;
-}
-
-outer();
-
-
----------------------
-
-console.log(foo()); // Typeerror : foo is not a function
-
-var foo = function() {
-  return "Hello, World!";
-};
-console.log(foo()); // Hello, World!
-
----------------------
-function createFunctions() {
-  var functions = [];
-  
-  for (var i = 0; i < 3; i++) {
-    functions[i] = function() {
-      console.log(i);
-    };
-  }
-  
-  return functions;
-}
-
-var funcs = createFunctions();
-funcs[0](); // 3
-funcs[1](); // 3
-funcs[2](); // 3
-
----------------------
-function createFunctions() {
-  var functions = [];
-  
-  for (let i = 0; i < 3; i++) {
-    functions[i] = function() {
-      console.log(i);
-    };
-  }
-  
-  return functions;
-}
-
-var funcs = createFunctions();
-funcs[0](); // 0
-funcs[1](); // 1
-funcs[2](); // 2
----------------------
-for (var i = 0; i < 3; i++) {
-  setTimeout(function() {
-    console.log(i);
-  }, 1000);
-}
-// output : 333
-
-for (let i = 0; i < 3; i++) {
-  setTimeout(function() {
-    console.log(i);
-  }, 1000);
-}
-// output : 012
----------------------
 async function foo() {
   console.log(1);
   await bar();
@@ -667,27 +599,8 @@ async function bar() {
 
 foo();
 // output : 132
----------------------
-console.log(a); // function return
-
-var a = 10;
-
-function a() {
-  console.log("Hello!");
-}
 
 ---------------------
-
----------------------
-
-
-------------
-
-
-
-
-
-
 
 function sayHi() {
     console.log(name);
@@ -698,24 +611,11 @@ function sayHi() {
   
   sayHi(); // undefined and ReferenceError
 
-
-
--------------------------
-for (var i = 0; i < 3; i++) {
-    setTimeout(() => console.log(i), 1);
-  } // 3 3 3 
-  
-  for (let i = 0; i < 3; i++) {
-    setTimeout(() => console.log(i), 1);
-  } // 0 1 2
-
 -------------------------
 
-
-+true; // 1
-!'Lydia'; // false
-
-
+console.log(+true); // 1
+console.log(!'Lydia'); // false - logical not operation return boolean value true and false
+console.log(+NaN);    // Output: NaN
 -------------------------
 const bird = {
     size: 'small',
@@ -733,7 +633,7 @@ const bird = {
 -------------------------
 
 let a = 3;
-let b = new Number(3);
+let b = new Number(3); // here type is object and b output is : Number{3}
 let c = 3;
 
 console.log(a == b); // true
@@ -1032,190 +932,4 @@ console.log(["a", "c", , "b"].sort()); // ['a', 'b', 'c', empty]
 console.log([, undefined, "a", "b"].sort()); // ["a", "b", undefined, empty]
 
 -------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
--------------------------
-
-
-
--------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
